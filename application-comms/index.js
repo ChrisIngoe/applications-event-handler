412b2cae69e7f1abcb6d0c286c426efea91274eb
Chris Ingoe <chris.software.engineer@gmail.com>
2022-01-27 21:24:45 +0000

    add application-comms-function

---
+++ application-comms/index.js
@@ -0,0 +1,24 @@
+exports.handler = async (event, context) => {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+
+  let body;
+  let statusCode = '200';
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    body = { status: 'Comms Complete' };
+  } catch (err) {
+    statusCode = '400';
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
--- application-sanction-check/index.js
+++ application-sanction-check/index.js
@@ -1,24 +1,43 @@
+const AWS = require('aws-sdk');
+const { EventBridgeClient, PutEventsCommand } = require('@aws-sdk/client-eventbridge');
+
 exports.handler = async (event, context) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
 
-  let body;
-  let statusCode = '200';
-  const headers = {
-    'Content-Type': 'application/json',
-  };
-
   try {
-    body = { status: 'Sanction Check Ok' };
+    event.detail.status = 'AWAITING_COMPLETION';
+    console.log(event);
+    await postNewApplicationEvent(event.detail);
+    console.log('Sanction Check Ok');
   } catch (err) {
-    statusCode = '400';
-    body = err.message;
-  } finally {
-    body = JSON.stringify(body);
+    console.error(err);
   }
 
-  return {
-    statusCode,
-    body,
-    headers,
-  };
+  return;
 };
+
+async function postNewApplicationEvent(event) {
+  const ebClient = new EventBridgeClient({ region: process.env.AWSREGION });
+
+  const entry = {
+    Entries: [
+      {
+        EventBusName: 'default',
+        Source: 'applicationsApiHandler.applications',
+        DetailType: 'subscription',
+        Detail: JSON.stringify(event),
+      },
+    ],
+  };
+
+  const command = new PutEventsCommand(entry);
+
+  try {
+    const data = await ebClient.send(command);
+    console.log('Success, event sent; requestID:', data);
+  } catch (err) {
+    console.log('Error', err);
+  }
+
+  return;
+}
